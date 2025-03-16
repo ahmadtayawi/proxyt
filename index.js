c2e2cb9b187eeb4d9a1f388b53a81c147490496d
MattiasBehnam <matte1753@gmail.com>
2025-03-16 13:22:27 +0100

    la till deltid endpoint

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 const JOB_SEARCH_API = 'https://jobsearch.api.jobtechdev.se/search';
 const JOB_LINKS_API = 'https://links.api.jobtechdev.se/joblinks';
 
+//https://jobsearch.api.jobtechdev.se/search?worktime-extent=947z_JGS_Uk2
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -64,6 +66,55 @@ app.get('/api/jobs', async (req, res) => {
     } catch (error) {
         console.error('Error fetching job links:', error.message);
         res.status(500).json({ error: 'Failed to fetch job links' });
+    }
+});
+
+
+//https://proxyserver-production-fe16.up.railway.app/api/jobs/deltid?q=deltid
+app.get('/api/jobs/deltid', async (req, res) => {
+    try {
+        if (Object.keys(req.query).length === 0) {
+            // No search parameters, use JOB_LINKS_API
+            const response = await axios.get(JOB_LINKS_API);
+            res.json(response.data);
+        } else {
+            // Search parameters exist, use JOB_SEARCH_API
+            let allJobs = [];
+            let page = 0;
+            const limit = 100;
+            let totalResults = 0;
+
+            do {
+                const response = await axios.get( JOB_LINKS_API, {
+                    params: {
+                        ...req.query,
+                        limit,
+                        offset: page * limit
+                    },
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                });
+
+                const jobLinks = response.data.hits || [];
+                allJobs = allJobs.concat(jobLinks);
+
+                if (page === 0) {
+                    totalResults = response.data.total.value || 0;
+                }
+
+                page++;
+
+            } while (allJobs.length < totalResults);
+
+            res.json(allJobs);
+        }
+    } catch (error) {
+        console.error('Error fetching job links:', error);
+        res.status(500).json({ 
+            error: 'Failed to fetch job links',
+            details: error.message 
+        });
     }
 });
 
